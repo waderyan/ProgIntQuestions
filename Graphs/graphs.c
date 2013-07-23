e62083d374df6c68a166cf5ed24a5246cfd2e7dd
Wade Anderson <wade.anderson7@gmail.com>
2013-07-22 21:26:04 -0600

    Graphs - partial solution.

---
+++ Graphs/graphs.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+dfs(graph *g, int v) {
+	
+	edgenode *p;
+	int y;
+
+	if (finished)
+		return;
+
+	discovered[v] = TRUE;
+	time = time + 1;
+	entry_time[v] = time;
+
+	process_vertex_early(v);
+
+	p = g
+	
+}
+
+
+
+
+
+main() {
+	printf("Hello World!");
+}
