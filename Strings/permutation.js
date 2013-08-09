197ed589152007e4a0059f8fe198117039695a51
Wade Anderson <wade.anderson7@gmail.com>
2013-08-09 08:08:18 -0600

    Javascript permutation

--- Strings/permutation.js
+++ Strings/permutation.js
@@ -1,2 +1,36 @@
 
-function permute(str, used, )
+function PermGenerator () {
+	this.perms = [];
+};
+
+PermGenerator.prototype.permutation = function(word) {
+	this.permute("", word);
+}
+
+PermGenerator.prototype.permute = function(prefix, str) {
+	var n = str.length;
+	if (!n) {
+		this.perms.push(prefix);
+	} else {
+		for (var i = 0; i < n; i++) {
+			this.permute(prefix + str[i], 
+				str.substr(0, i) + str.substr(i+1, n)
+			);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
